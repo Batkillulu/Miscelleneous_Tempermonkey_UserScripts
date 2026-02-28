2e2f52b68fa0345a52141bdfa9f2e7befc64a1f6
Batkillulu <baptjm69@gmail.com>
2026-02-28 12:10:07 +0100

    fix: remove unused grid template column style from layout

--- Wider-MDN.user.js
+++ Wider-MDN.user.js
@@ -14,7 +14,6 @@
     'use strict';
     // Your code here...
     document.querySelector(".layout__2-sidebars-inline.reference-layout").style.paddingInline = "10px";
-    // document.querySelector(".layout__2-sidebars-inline.reference-layout").style.gridTemplateColumns = '[full-start left-sidebar-start] 15% [left-sidebar-end] 1% [content-start] 71% [content-end] 1% [right-sidebar-start] 12% [full-end right-sidebar-end]';
     document.querySelector(".layout__right-sidebar.reference-layout__toc").style.justifySelf = "right";
     document.querySelector(".layout__right-sidebar.reference-layout__toc").style.marginRight = "0px";
     document.querySelector(".layout__right-sidebar.reference-layout__toc").style.width = "100%";
