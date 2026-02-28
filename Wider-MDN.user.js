6752a5c6bdfe5c69b0cd619829e086a7f3674bad
Batkillulu <baptjm69@gmail.com>
2026-02-28 12:05:54 +0100

    improved MDN's display

--- Wider-MDN.user.js
+++ Wider-MDN.user.js
@@ -14,7 +14,10 @@
     'use strict';
     // Your code here...
     document.querySelector(".layout__2-sidebars-inline.reference-layout").style.paddingInline = "10px";
+    // document.querySelector(".layout__2-sidebars-inline.reference-layout").style.gridTemplateColumns = '[full-start left-sidebar-start] 15% [left-sidebar-end] 1% [content-start] 71% [content-end] 1% [right-sidebar-start] 12% [full-end right-sidebar-end]';
     document.querySelector(".layout__right-sidebar.reference-layout__toc").style.justifySelf = "right";
-    document.querySelector(".layout__header.reference-layout__header").style.width = "108%";
+    document.querySelector(".layout__right-sidebar.reference-layout__toc").style.justifySelf = "right";
+    document.querySelector(".layout__right-sidebar.reference-layout__toc").style.width = "100%";
     document.documentElement.style.setProperty("--layout-content-max", "64rem");
+    document.documentElement.style.setProperty("--layout-2-sidebars", '[full-start left-sidebar-start] 15% [left-sidebar-end] 1% [content-start] 71% [content-end] 1% [right-sidebar-start] 12% [full-end right-sidebar-end]')
 })();
